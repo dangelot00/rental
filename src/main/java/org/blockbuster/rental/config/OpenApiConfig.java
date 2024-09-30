53593a33616092de5018b1f9c8be0ee265dac516
Thomas D'Angelo <dangelot00@gmail.com>
2024-09-30 22:41:41 +0200

    added security description in OpenApiConfig

--- src/main/java/org/blockbuster/rental/config/OpenApiConfig.java
+++ src/main/java/org/blockbuster/rental/config/OpenApiConfig.java
@@ -1,9 +1,15 @@
 package org.blockbuster.rental.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -11,9 +17,18 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Film Rental API")
-                        .description("API documentation for the Film Rental application")
-                        .version("v1.0"));
+                    .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                            new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                    )
+                    .security(List.of(new SecurityRequirement().addList("bearerAuth")))
+                    .info(new Info()
+                            .title("Film Rental API")
+                            .description("API documentation for the Film Rental application")
+                            .version("v1.0"));
     }
 }
